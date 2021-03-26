d3c44f6bd35a1656c96b0bb5c1d6143a82002a69
Eliaxie <elia.maggioni@gmail.com>
2021-03-26 10:03:23 +0100

    ActionCardDeck update

--- src/main/java/it.polimi.ingsw.model/ActionCardDeck.java
+++ src/main/java/it.polimi.ingsw.model/ActionCardDeck.java
@@ -2,21 +2,35 @@ package it.polimi.ingsw.model;
 
 import java.util.*;
 
+/**
+ *
+ */
 public class ActionCardDeck {
+
     public ActionCardDeck(){
-        usedStack = new LinkedList<ActionCard>();
-        availableStack = new LinkedList<ActionCard>();
+        usedStack = new Stack<ActionCard>();
+        availableStack = new Stack<ActionCard>();
         /* TODO */
     }
-    private LinkedList<ActionCard> usedStack;
-    private LinkedList<ActionCard> availableStack;
+
+    private Stack<ActionCard> usedStack;
+    private Stack<ActionCard> availableStack;
+
+    /**
+     *
+     * @return ActionCard
+     */
     public ActionCard getCard(){return null;}
+
+    /**
+     *
+     */
     public void shuffleDeck(){
-        LinkedList<ActionCard> temp = new LinkedList<ActionCard>();
+        Stack<ActionCard> temp = new Stack<ActionCard>();
         temp.addAll(0, usedStack);
         temp.addAll(0, availableStack);
         Collections.shuffle(temp);
         usedStack = temp;
-        availableStack = new LinkedList<ActionCard>();
+        availableStack = new Stack<ActionCard>();
     }
 }
