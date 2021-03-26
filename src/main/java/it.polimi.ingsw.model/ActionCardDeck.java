d65879fad1537cb91fcdfd979bef6dcb3ac4c69c
Eliaxie <elia.maggioni@gmail.com>
2021-03-26 09:23:47 +0100

    ActionCardDeck added methods

--- src/main/java/it.polimi.ingsw.model/ActionCardDeck.java
+++ src/main/java/it.polimi.ingsw.model/ActionCardDeck.java
@@ -1,13 +1,22 @@
 package it.polimi.ingsw.model;
 
-import java.util.List;
+import java.util.*;
 
 public class ActionCardDeck {
-    private List<ActionCard> usedStack;
-    private List<ActionCard> availableStack;
-    public ActionCard getCard();
+    public ActionCardDeck(){
+        usedStack = new LinkedList<ActionCard>();
+        availableStack = new LinkedList<ActionCard>();
+        /* TODO */
+    }
+    private LinkedList<ActionCard> usedStack;
+    private LinkedList<ActionCard> availableStack;
+    public ActionCard getCard(){return null;}
     public void shuffleDeck(){
-        List<ActionCard> temp = new List<ActionCard>();
+        LinkedList<ActionCard> temp = new LinkedList<ActionCard>();
         temp.addAll(0, usedStack);
-    };
+        temp.addAll(0, availableStack);
+        Collections.shuffle(temp);
+        usedStack = temp;
+        availableStack = new LinkedList<ActionCard>();
+    }
 }
