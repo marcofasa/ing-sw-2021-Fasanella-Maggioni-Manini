fbb256a7b9efb6e2afbe18829e25c23a4f2ffb97
Eliaxie <elia.maggioni@gmail.com>
2021-03-26 09:06:56 +0100

    uml update

--- src/main/java/it.polimi.ingsw.model/ActionCardDeck.java
+++ src/main/java/it.polimi.ingsw.model/ActionCardDeck.java
@@ -1,5 +1,13 @@
 package it.polimi.ingsw.model;
 
+import java.util.List;
+
 public class ActionCardDeck {
-    private ActionCard actionCard;
+    private List<ActionCard> usedStack;
+    private List<ActionCard> availableStack;
+    public ActionCard getCard();
+    public void shuffleDeck(){
+        List<ActionCard> temp = new List<ActionCard>();
+        temp.addAll(0, usedStack);
+    };
 }
