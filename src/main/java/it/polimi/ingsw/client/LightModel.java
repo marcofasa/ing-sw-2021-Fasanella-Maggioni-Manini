b2d9f505a7f22d1f455590cf47029b32d9111e4a
marcofasa <MRfs080899>
2021-05-11 23:06:37 +0200

    Implementing CLI

--- src/main/java/it/polimi/ingsw/client/LightModel.java
+++ src/main/java/it/polimi/ingsw/client/LightModel.java
@@ -1,8 +1,6 @@
 package it.polimi.ingsw.client;
 
-import it.polimi.ingsw.communication.client.RequestMarketUse;
 import it.polimi.ingsw.model.*;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,9 +8,7 @@ public class LightModel {
     private String nickname;
     private Client client;
     private int numberOfPlayers;
-    private boolean first;
     private HashMap<String, Integer> playersPosition;
-    private PlayerState playerState;
     private ArrayList<ArrayList<CardDevelopment>> cardDevelopmentMarket;
     private ArrayList<CardDevelopment> cardDevelopment;
     private ArrayList<CardLeader> cardsLeader;
@@ -21,32 +17,69 @@ public class LightModel {
     private HashMap<Resource, Integer> strongbox;
     private ArrayList<ArrayList<MarbleType>> market;
 
-    public ArrayList<CardLeader> getCardsLeader() {
-        return cardsLeader;
+
+    //Light Version of the Model, with only the essential parts useful to View
+
+    /**
+     * Constructor
+     * @param client
+     */
+    public LightModel(Client client){
+        this.client=client;
     }
+
+
 
     public int positionPlayer(){
         return playersPosition.get(nickname);
     }
 
+    //Setters (used as Update methods)
+
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
 
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setFirst(boolean first) {
-        this.first = first;
-    }
-
-    public void setPlayerState(PlayerState playerState) {
-        this.playerState = playerState;
-    }
 
     public void setNumberOfPlayers(int numberOfPlayers) {
         this.numberOfPlayers = numberOfPlayers;
+    }
+
+    public void setCardDevelopmentMarket(ArrayList<ArrayList<CardDevelopment>> cardDevelopmentMarket){
+        this.cardDevelopmentMarket= new ArrayList<ArrayList<CardDevelopment>>(cardDevelopmentMarket);
+    }
+    public void setCardsLeader(ArrayList<CardLeader> cardsLeader) {
+        this.cardsLeader = new ArrayList<CardLeader>(cardsLeader);
+    }
+
+    public void setCardDevelopment(ArrayList<CardDevelopment> cardDevelopment) {
+        this.cardDevelopment = new ArrayList<CardDevelopment>(cardDevelopment);
+    }
+
+    public void setMarket(ArrayList<ArrayList<MarbleType>> market) {
+        this.market = new ArrayList<ArrayList<MarbleType>>(market);
+    }
+
+    public void setStrongbox(HashMap<Resource, Integer> strongbox) {
+        this.strongbox = new HashMap<Resource, Integer>(strongbox);
+    }
+
+    public void setPlayersPosition(HashMap<String, Integer> playersPosition) {
+        this.playersPosition = new HashMap<String, Integer>(playersPosition);
+    }
+
+    public void setDeposit(HashMap<Resource, Integer> deposit) {
+        this.deposit = new HashMap<Resource, Integer>(deposit);
+    }
+
+    public void setTileStatuses(ArrayList<FaithTileStatus> tileStatuses) {
+        this.tileStatuses = new ArrayList<FaithTileStatus>(tileStatuses);
+    }
+
+
+    //Getters
+    public String getNickname() {
+        return nickname;
     }
 
     public int getNumberOfPlayers() {
@@ -57,7 +90,7 @@ public class LightModel {
         if (market==null) market=new ArrayList<ArrayList<MarbleType>>();
         /*
         try {
-            market= client.sendAndWait(new RequestMaketInstance(),-1);
+            client.sendAndWait(new RequestMarketInstance(),-1);
         }
         catch (RequestTimeoutException e){
             e.printStackTrace();
@@ -68,16 +101,12 @@ public class LightModel {
     }
 
 
-
-    public LightModel(Client client){
-        this.client=client;
-    }
-
     public ArrayList<ArrayList<CardDevelopment>> getCardDevelopmentMarket() {
         if (cardDevelopmentMarket==null) cardDevelopmentMarket= new ArrayList<ArrayList<CardDevelopment>>();
        /*
        try {
-            cardDevelopmentMarket= client.sendAndWait(new RequestCardDevelopmentMarketInstance(),-1);
+            client.sendAndWait(new RequestCardDevelopmentMarketInstance(),-1);
+
         }
         catch (RequestTimeoutException e){
             e.printStackTrace();
@@ -90,7 +119,8 @@ public class LightModel {
         if(tileStatuses==null) tileStatuses=new ArrayList<FaithTileStatus>();
         /*
         try {
-            tileStatuses= client.sendAndWait(new RequestTileStatusesInstance(),-1);
+            client.sendAndWait(new RequestTileStatusesInstance(),-1);
+
         }
         catch (RequestTimeoutException e){
             e.printStackTrace();
@@ -104,6 +134,7 @@ public class LightModel {
         /*
         try {
             playersPosition= client.sendAndWait(new RequestPlayersPositionInstance(),-1);
+            return playersPosition;
         }
         catch (RequestTimeoutException e){
             e.printStackTrace();
@@ -111,6 +142,58 @@ public class LightModel {
         */
 
         return playersPosition;
+    }
+
+    public HashMap<Resource, Integer> getDeposit(){
+        if (deposit==null) deposit=new HashMap<Resource,Integer>();
+        /*
+        try {
+            client.sendAndWait(new RequestDepositIstance(),-1);
+        }catch (RequestTimeoutException e){
+            e.printStackTrace();
+        }
+
+         */
+        return deposit;
+    }
+
+    public ArrayList<CardLeader> getCardsLeader() {
+        if(cardsLeader==null) cardsLeader= new ArrayList<CardLeader>();
+        /*
+        try {
+            client.sendAndWait(new RequestCardLeaderInstance(),-1);
+        }catch (RequestTimeoutException e){
+            e.printStackTrace();
+        }
+
+         */
+        return cardsLeader;
+    }
+
+    public ArrayList<CardDevelopment> getCardDevelopment() {
+        if(cardDevelopment==null) cardDevelopment= new ArrayList<CardDevelopment>();
+        /*
+        try {
+            client.sendAndWait(new RequestCardDevelopmentInstance(),-1);
+        }catch (RequestTimeoutException e){
+            e.printStackTrace();
+        }
+
+         */
+        return cardDevelopment;
+    }
+
+    public HashMap<Resource, Integer> getStrongbox() {
+        if(strongbox==null) strongbox= new HashMap<>();
+        /*
+        try {
+            client.sendAndWait(new RequestStrongboxInstance(),-1);
+        }catch (RequestTimeoutException e){
+            e.printStackTrace();
+        }
+
+         */
+        return strongbox;
     }
 
 }
