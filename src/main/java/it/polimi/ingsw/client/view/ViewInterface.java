831697373c33ea9a9cf09416422651e3c0562326
marcofasa <marco.fasanella@mail.polimi.it>
2021-05-15 19:37:31 +0200

    Recover (real account) (complete). Pushed lines not counted because intelliJ used a pseudo-account to commit. This push is used to recover all line counts of my real account

---
+++ src/main/java/it/polimi/ingsw/client/view/ViewInterface.java
@@ -0,0 +1,122 @@
+package it.polimi.ingsw.client.view;
+
+import it.polimi.ingsw.model.CardLeader;
+import it.polimi.ingsw.model.Marble;
+import it.polimi.ingsw.model.Resource;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface ViewInterface {
+    //DISPLAY
+
+    /*
+    Display Welcome Message
+     */
+    void displayWelcome();
+
+    /*
+    Display Match Starting
+     */
+    void displayStartingGame();
+
+    /*
+    Display Message
+     */
+    void displayMessage(String message);
+
+
+    /*
+    Display Position in FaithTrail
+     */
+    void displayPosition();
+
+
+    void displayTimeOut();
+
+    /*
+    Display Market
+     */
+    void displayMarket();
+
+    /*
+    Display StrongBox
+     */
+    void displayStrongBox();
+
+    /*
+    Display that the Player is not active
+     */
+    void displayNotActivePlayerError();
+
+    /*
+    Display that there are not enough resources to continue action
+     */
+    void displayNotEnoughResource();
+
+    /*
+    Display Disconnection
+     */
+    void displayDisconnection();
+
+    /*
+    Display Win Message
+     */
+    void displayWin();
+
+    /*
+    Display Lost Message
+     */
+    void displayLost();
+
+    /*
+    Display successful action
+     */
+    void displaySuccess();
+
+    /*
+    Display requirements not met
+     */
+    void displayLeaderRequirementsNotMet();
+
+    /*
+    Display
+     */
+    void displayTurn(String currentPlayer);
+
+    void displayWaitingOpponent(String currentPlayer);
+
+    void displayDeposit();
+    void displayCardLeader();
+    void displayCardDevelopment();
+
+    //ASK
+
+    //REQUEST
+
+    String askNickName();
+
+    int askPlayerNumber();
+
+    void askMarketChoice();
+
+    void askDevelopmentCardChoice();
+
+    void askProductionActivation();
+
+    void askCardLeaderActivation();
+
+    void askEndTurn();
+
+    //RESPONSE
+
+    ArrayList<Resource> askForInitialResourcesSelection(int playerNumber);
+
+    ArrayList<CardLeader> askForLeaderCardSelection(ArrayList<CardLeader> cardLeaders);
+
+    HashMap<Resource,Integer> askForResourceToDiscard(HashMap<Resource,Integer> choice);
+
+    void displayWaiting(int timeoutInSeconds);
+
+    void askCardLeaderDiscard();
+}
